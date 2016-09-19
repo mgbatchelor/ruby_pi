1a9353639849d56272cfa502c97ab6f4c7eea707
Michael Batchelor <batchelor@nationbuilder.com>
2016-09-18 18:18:12 -0700

    simplify run

--- run.rb
+++ run.rb
@@ -1,6 +1,4 @@
 require_relative 'lib/ruby_pi'
 require 'rpi_gpio'
 RPi::GPIO.set_numbering :bcm
-scanner = Scanner::Console.new
-processor = Processor::Gpio.new(left: 20, right: 21)
-Runner.new(Brain.new, scanner, processor).run
+Runner.new.run
